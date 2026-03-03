e25fed166927cdc56f3178ef75daa08b52b3e99a
NIRMALKUMAR223 <rockerrock223@gmail.com>
2026-03-03 15:49:12 +0530

    update

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -4,6 +4,12 @@ const products = [
     name: "MagSafe Wallet",
     image: "/img/product1.PNG",
     link: "https://amzn.to/3OJ2pBg"
+  },
+  {
+    id: 2,
+    name: "Nike Revolution 7 Running Shoes",
+    image: "/img/product2.jpeg",
+    link: "https://amzn.to/4rMzDOP"
   }
 ];
 
